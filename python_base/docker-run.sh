c003ff8ad7010d1d03ae009249093653c4fa9e00
anorakj <nicholasjcc@gmail.com>
2021-01-18 16:09:49 +0800

    add python base

--- python_base/docker-run.sh
+++ python_base/docker-run.sh
@@ -1,7 +1,7 @@
 docker run \
 -dp 8888:8888 \
--w /Users:/$USER \
+-w /Users/$USER \
 --mount type=bind,source=/Users,target=/Users \
-pdp_crawler \
+pythonbase \
 --port=8888 \
 --notebook-dir=/Users/$USER
